1d64342cdc99db8fc366d7f34fa9456cb0f177b2
gabriele <gabriele@universumas.com>
2024-07-10 12:11:21 +0300

    Add create word method

--- backend/anagram_searcher/models.py
+++ backend/anagram_searcher/models.py
@@ -4,3 +4,13 @@ from django.db import models
 class Word(models.Model):
     word = models.CharField(max_length=100, unique=True)
     canonical_form = models.CharField(max_length=100, db_index=True)
+
+    @staticmethod
+    def add_list_of_words(words):
+        if not words:
+            return
+
+        Word.objects.bulk_create(
+            [Word(word=word, canonical_form=''.join(sorted(word))) for word in words],
+            ignore_conflicts=True
+        )
